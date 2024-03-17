088dfba2149c33c24591c16fe39245a50a1721d7
farhanmuzakki213 <farhanmuzakki1p@gmail.com>
2024-03-17 18:56:09 +0700

    fronten: ubah tampilan footer

--- resources/views/frontend/body/footer.blade.php
+++ resources/views/frontend/body/footer.blade.php
@@ -1,15 +1,38 @@
-<footer class="footer py-4">
-    <div class="container">
+<footer class="footer py-5">
+    <div class="container-fruid">
         <div class="row align-items-center">
-            <div class="col-lg-4 text-lg-start">Copyright &copy; Your Website 2023</div>
-            <div class="col-lg-4 my-3 my-lg-0">
-                <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Twitter"><i class="fab fa-twitter"></i></a>
-                <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Facebook"><i class="fab fa-facebook-f"></i></a>
-                <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="LinkedIn"><i class="fab fa-linkedin-in"></i></a>
+            <div class="col-md-5 text-center text-md-left">
+                <img src="{{ asset('frontend/landing-page/assets/img/logoti3-white.png') }}" alt="Footer Image"
+                    class="img-fluid mb-2 mb-md-0">
             </div>
-            <div class="col-lg-4 text-lg-end">
-                <a class="link-dark text-decoration-none me-3" href="#!">Privacy Policy</a>
-                <a class="link-dark text-decoration-none" href="#!">Terms of Use</a>
+            <div class="col-md-7">
+                <div class="row text-start text-light">
+                    <div class="col-md-4">
+                        <h5>Program Studi</h5>
+                        <ul class="list-unstyled">
+                            <li>D-IV Teknologi Rekayasa Perangkat Lunak</li>
+                            <li>D-IV Animasi</li>
+                            <li>D-III Manajemen Informatika</li>
+                            <li>D-III Teknik Komputer</li>
+                            <li>D-III Sistem Informasi</li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3">
+                        <h5>About</h5>
+                        <ul class="list-unstyled">
+                            <li>Tentang Teknologi Informasi</li>
+                            <li>Staf KBK</li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3">
+                        <h5>Connection</h5>
+                        <ul class="list-unstyled">
+                            <li><i class="fa-regular fa-envelope"></i> <a class="text-light" href="#" style="text-decoration:none">KBKTI@gmail.com</a></li>
+                            <li><i class="fa-brands fa-whatsapp"></i> <a class="text-light" href="#" style="text-decoration:none">08457392793</a></li>
+                            <li><i class="fa-solid fa-location-dot"></i> <a class="text-light" href="https://maps.app.goo.gl/2pmCxQ4vPy6f6fZp6" style="text-decoration:none">Teknologi Informasi</a></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
